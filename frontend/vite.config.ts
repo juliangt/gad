80c3855d32e735970543ae3c5cf15b5fc6006fa6
Julián Garcia Tuñón <juliangarciatunon@gmail.com>
2026-07-10 20:11:52 -0300

    perf(frontend): optimize bundle by configuring manualChunks (resolves #16)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -29,4 +29,15 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router-dom'],
+          query: ['@tanstack/react-query'],
+          map: ['leaflet', 'react-leaflet'],
+        },
+      },
+    },
+  },
 });
